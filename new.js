64db46a0487a7a336bdc0826601c51fa025d8d24
Dev-dfm <d.floresmedina@gmail.com>
2021-04-26 16:02:13 +0200

    Add submitButton click listener

---
+++ new.js
@@ -0,0 +1,11 @@
+// alert("Show Task");
+
+// Variables
+const submitButton = document.querySelector(".form__submit");
+
+//Events
+submitButton.addEventListener("click", showTask);
+
+function showTask() {
+    alert("Show task");
+  }
