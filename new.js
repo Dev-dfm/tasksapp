be0c3f7d4ecaa1d7daba70384a58123d0b346e38
Dev-dfm <d.floresmedina@gmail.com>
2021-04-28 16:37:19 +0200

    Add updating array

--- new.js
+++ new.js
@@ -6,7 +6,6 @@
 //Events
 // submitButton.addEventListener("click", showTask);
 
-
 //Zwischenspeichern der Variable enterTask in taskNameInput (Elemente aus dem DOM zwischenspeichern)
 // Query elements from DOM
 const formElement = document.querySelector(".form");
@@ -18,7 +17,6 @@ formElement.onsubmit = function (event) {
   const taskNameInput = document.querySelector(".form__enterTask");
   const dateInput = document.querySelector(".radiocontainer__input:checked");
 
-
   //mit ! wird überprüft ob die Bedingung false ist
   if (!taskNameInput.value) {
     alert("Please enter a task");
@@ -28,12 +26,32 @@ formElement.onsubmit = function (event) {
   if (!dateInput) {
     alert("Please select a date");
     return; //Beendet den Code an dieser Stelle
-  } 
+  }
 
   const task = {
     name: taskNameInput.value,
     date: dateInput.value,
-  }
+  };
 
-  console.log(task);
+/////// OLD PART ///////////////////////////
+  //aus task ein JSON/String machen
+  // const taskJSON = JSON.stringify(task);
+  //im "local storage" des Browsers speichern
+  // localStorage.setItem("task", taskJSON);
+ ////////////////////////////////////////////
+
+// get values of the key "taskList" from the browser-storage
+const taskListJSON = localStorage.getItem("taskList");
+// convert/parse this value to an object
+const taskList = JSON.parse(taskListJSON);
+
+// add/push new task to existing taskList
+taskList.push(task);
+
+//convert the taskList to JSON/String
+const newTaskListJSON = JSON.stringify(taskList);
+// overwrite existing taskList with updated newTaskListJSON 
+localStorage.setItem("taskList", newTaskListJSON);
+
+console.log(taskList);
 };
